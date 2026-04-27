95ae465a4c05c9000be97566739d2993eb782dc8
Felipe Pereira da Silva <felipep21silva@gmail.com>
2026-04-27 14:51:53 -0300

    Mais Ajustes

--- financas_projeto/financas_projeto/settings.py
+++ financas_projeto/financas_projeto/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'usuarios', 'static', 'usuarios')
+    os.path.join(BASE_DIR, 'usuarios', 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
